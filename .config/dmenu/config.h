26e1580a10a12e4f793bc71a6720fbc3762450b3
p0lygun <solankivibhakar82@gmail.com>
2022-04-06 01:17:03 +0530

    changed theme

--- .config/dmenu/config.h
+++ .config/dmenu/config.h
@@ -7,10 +7,16 @@ static const char *fonts[] = {
 	"monospace:size=12", "Noto Color Emoji:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+/* custom color scheme https://colorhunt.co/palette/151d3bd821486ebf8bdadbbd */
+static const char col_navy[]        = "#151D3B";
+static const char col_red[]        = "#D82148";
+static const char col_teal[]        = "#6EBF8B";
+static const char col_beige[]        = "#DADBBD";
+
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	        	/*     fg         bg       */
+	[SchemeNorm] = {col_beige, col_navy },
+	[SchemeSel] = { col_red, col_navy },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
