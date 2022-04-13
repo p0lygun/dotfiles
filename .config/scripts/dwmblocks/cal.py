c040e59f62ec84f1c08c291ed061b78b7c1de6a5
p0lygun <solankivibhakar82@gmail.com>
2022-04-13 23:26:18 +0530

    remove extra scripts

--- .config/scripts/dwmblocks/cal.py
+++
@@ -1,8 +0,0 @@
-import calendar
-import os
-from datetime import datetime
-d = datetime.now()
-cal = repr(calendar.month(d.year, d.month))
-if os.getenv('BUTTON') == '1':
-    os.system(f"dunstify 'Calander' {cal}")
-print(d.strftime("%b %d (%a) %I:%M:%S %p "))
